83886b2d9076cec4ea94f580aa0cec68e33923b3
Jon SlinnHawkins <jon@slinnhawkins.com>
2019-03-10 20:45:03 +0000

    zonename undefined check. (#2)
    * changed name to reflect project name.
    
    * typeof check for undefined zonename.
    
    * Typeof check for undefined zonename

--- roon-mqtt.js
+++ roon-mqtt.js
@@ -146,12 +146,15 @@ var roon = new RoonApi({
 						for( var index in zones ) {
 							var zonedata = roonzone_json_changeoutputs(zones[index]);
 							var zonename = zonedata.display_name;
-							//var regex = '';
-							zonename = zonename.replace(/ \+.*/,'');
-							roon_zones[zonename] = JSON.parse(JSON.stringify(zonedata));
-							console.log('sending state for zone %s', zonename);
-							for ( var attribute in zonedata ) {
-								mqtt_publish_JSON( 'roon/'+zonename, mqtt_client, zonedata);
+							
+							if (typeof zonename !== 'undefined' && zonename) {
+								//var regex = '';
+								zonename = zonename.replace(/ \+.*/,'');
+								roon_zones[zonename] = JSON.parse(JSON.stringify(zonedata));
+								console.log('sending state for zone %s', zonename);
+								for ( var attribute in zonedata ) {
+									mqtt_publish_JSON( 'roon/'+zonename, mqtt_client, zonedata);
+								}
 							}
 						}
 					}
