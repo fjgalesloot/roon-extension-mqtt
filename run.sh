399ac18458f26c0eadafe0b69cfd115385d3781d
Floris Jan Galesloot <fjgalesloot@vloeiendit.nl>
2020-07-24 21:51:49 +0200

    Change to LF and bash

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-until env DEBUG="*" node ./roon-mqtt.js; do
+#!/bin/bash
+until env DEBUG="*" node ./roon-mqtt.js ; do
     echo "roon-extension-mqtt terminated with exit code $?.  Restarting.." >&2
     sleep 1
 done
